a22ddb4b7b1d0ec657676c317827bf984cd955b6
Md Wahid Rahman <60522937+WahidRahman123@users.noreply.github.com>
2024-07-05 21:43:13 +0600

    Add sql file

---
+++ lms_db.sql
@@ -0,0 +1,329 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2022 at 04:26 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `admin_email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `admin_pass` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_email`, `admin_pass`) VALUES
+(1, 'Admin Kumar', 'admin@gmail.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `categoryName` varchar(255) NOT NULL,
+  `Status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `categoryName`, `Status`) VALUES
+(1, 'Programming Languages', 1),
+(2, 'Music', 1),
+(4, 'Singing', 1),
+(5, 'Acting', 1),
+(6, 'Business', 1),
+(7, 'Design', 1),
+(8, 'Marketing', 1),
+(9, 'Lifestyle', 1),
+(10, 'Development', 1),
+(11, 'Office Productivity', 0),
+(12, 'Photography and Video', 1),
+(13, 'Personal Development', 1),
+(14, 'Health and Fitness', 1),
+(15, 'Teaching and Academics', 1),
+(16, 'Project Management', 1),
+(17, 'Layout Design', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` int(11) NOT NULL,
+  `course_name` text COLLATE utf8_bin NOT NULL,
+  `course_desc` text COLLATE utf8_bin NOT NULL,
+  `course_author` varchar(255) COLLATE utf8_bin NOT NULL,
+  `course_img` text COLLATE utf8_bin NOT NULL,
+  `course_duration` text COLLATE utf8_bin NOT NULL,
+  `course_price` int(11) NOT NULL,
+  `course_original_price` int(11) NOT NULL,
+  `category` varchar(255) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_id`, `course_name`, `course_desc`, `course_author`, `course_img`, `course_duration`, `course_price`, `course_original_price`, `category`) VALUES
+(9, 'Complete PHP Bootcamps', 'This course will help you get all the Object Oriented PHP, MYSQLi and ending the course by building a CMS System.', 'Wahid Rahman', '../image/courseimg/php.jpg', '3 Months', 800, 1900, 'Programming Languages'),
+(10, 'Learn Python A-Z', 'This is the most comprehensive, yet straight-forward, course for the Python programming language.', 'Mehedi', '../image/courseimg/ai.jpg', '4 Months', 800, 1800, NULL),
+(11, 'Hands-on Artificial Intelligence', 'Learn and Master how AI works and how it is changing our lives in this Course.\r\nlives in this Course.', 'Maisha', '../image/courseimg/Machine.jpg', '6 Months', 900, 1900, NULL),
+(12, 'Learn Vue JS', 'The skills you will learn from this course is applicable to the real world, so you can go ahead and build similar app.', 'Maliha', '../image/courseimg/vue.jpg', '2 Months', 100, 1000, NULL),
+(13, 'Angular JS', 'Angular is one of the most popular frameworks for building client apps with HTML, CSS and TypeScript.', 'Jahan', '../image/courseimg/angular.jpg', '4 Month', 800, 1600, NULL),
+(16, 'Python Complete', 'This is complete Python COurse', 'Ruku', '../image/courseimg/Python.jpg', '4 hours', 500, 4000, NULL),
+(17, 'Learn React Native', 'THis is react native for android and iso app development', 'GeekyShows', '../image/courseimg/Machine.jpg', '2 months', 200, 3000, NULL),
+(18, 'HTML Learning Course', 'Build Responsive Real-World Websites with HTML', 'Mehedi', '../image/courseimg/HTML.jpg', '10 Hours', 2000, 10000, NULL),
+(19, 'Learn Java', 'Java is a powerful Language.', 'Raza', '../image/courseimg/', '5h', 2000, 5500, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courseorder`
+--
+
+CREATE TABLE `courseorder` (
+  `co_id` int(11) NOT NULL,
+  `order_id` varchar(255) COLLATE utf8_bin NOT NULL,
+  `stu_email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `status` varchar(255) COLLATE utf8_bin NOT NULL,
+  `respmsg` text COLLATE utf8_bin NOT NULL,
+  `amount` int(11) NOT NULL,
+  `order_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `courseorder`
+--
+
+INSERT INTO `courseorder` (`co_id`, `order_id`, `stu_email`, `course_id`, `status`, `respmsg`, `amount`, `order_date`) VALUES
+(8, 'ORDS22968322', 'mario@ischool.com', 10, 'TXN_SUCCESS', 'Txn Success', 800, '2019-09-13'),
+(9, 'ORDS78666589', 'ignou@ischool.com', 10, 'TXN_SUCCESS', 'Txn Success', 800, '2019-09-19'),
+(10, 'ORDS59885531', 'sonam@gmail.com', 10, 'TXN_SUCCESS', 'Txn Success', 800, '2020-07-04'),
+(15, 'ORDS46215624', 'mehedi@gmail.com', 9, 'Success', 'Done', 800, '2030-10-22'),
+(17, 'ORDS74921625', 'mehedi@gmail.com', 11, 'Success', 'Done', 900, '2022-10-31'),
+(18, 'ORDS33343328', 'wahid@gmail.com', 10, 'Success', 'Done', 800, '2022-11-13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `f_id` int(11) NOT NULL,
+  `f_content` text COLLATE utf8_bin NOT NULL,
+  `status` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `stu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`f_id`, `f_content`, `status`, `stu_id`) VALUES
+(3, 'My life at iSchool made me stronger and took me a step ahead for being an independent women. I am thankful to all the teachers who supported us and corrected us throughout our career. I am very grateful to the iSchool for providing us the best of placement opportunities and finally I got placed in DC Marvel.', '1', 171),
+(8, 'I am grateful to iSchool - both the faculty and the Training & Placement Department. They have made efforts ensuring maximum number of placed students. Due to the efforts made by the faculty and placement cell. I was able to bag a job in the second company.', '1', 172),
+(9, 'iSchool is a place of learning, fun, culture, lore, literature and many such life preaching activities. Studying at the iSchool brought an added value to my life.', '1', 173),
+(10, 'Think Magical, that is one thing that iSchool urges in and to far extent succeed in teaching to its students which invariably helps to achieve what you need.', '1', 174),
+(12, 'Knowledge is power. Information is liberating. Education is the premise of progress, in every society, in every family.', '1', 180),
+(13, 'This is Awesome GeekySHows Jindabaad', '1', 182),
+(14, 'I have bought some courses and they are amazing!!!', '1', 183),
+(15, 'This site helped us a lot!!!', '0', 185);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lesson`
+--
+
+CREATE TABLE `lesson` (
+  `lesson_id` int(11) NOT NULL,
+  `lesson_name` text COLLATE utf8_bin NOT NULL,
+  `lesson_desc` text COLLATE utf8_bin NOT NULL,
+  `lesson_link` text COLLATE utf8_bin NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `course_name` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `lesson`
+--
+
+INSERT INTO `lesson` (`lesson_id`, `lesson_name`, `lesson_desc`, `lesson_link`, `course_id`, `course_name`) VALUES
+(32, 'Introduction to Python ', 'Introduction to Python Desc', '../lessonvid/video2.mp4', 10, 'Learn Python A-Z'),
+(33, 'How Python Works', 'How Python Works Descc', '../lessonvid/video3.mp4', 10, 'Learn Python A-Z'),
+(34, 'Why Python is powerful', 'Why Python is powerful Desc', '../lessonvid/video9.mp4', 10, 'Learn Python A-Z'),
+(35, 'Everyone should learn Python ', 'Everyone should learn Python  Desccc', '../lessonvid/video1.mp4', 10, 'Learn Python A-Z'),
+(36, 'Introduction to PHP', 'Introduction to PHP Desc', '../lessonvid/video4.mp4', 9, 'Complete PHP Bootcamp'),
+(37, 'How PHP works', 'How PHP works Desc', '../lessonvid/video5.mp4', 9, 'Complete PHP Bootcamp'),
+(38, 'is PHP really easy ?', 'is PHP really easy ? desc', '../lessonvid/video6.mp4', 9, 'Complete PHP Bootcamp'),
+(39, 'Introduction to Guitar44', 'Introduction to Guitar desc1', '../lessonvid/video7.mp4', 8, 'Learn Guitar The Easy Way'),
+(40, 'Type of Guitar', 'Type of Guitar Desc2', '../lessonvid/video8.mp4', 8, 'Learn Guitar The Easy Way'),
+(41, 'Intro Hands-on Artificial Intelligence', 'Intro Hands-on Artificial Intelligence desc', '../lessonvid/video10.mp4', 11, 'Hands-on Artificial Intelligence'),
+(42, 'How it works', 'How it works descccccc', '../lessonvid/video11.mp4', 11, 'Hands-on Artificial Intelligence'),
+(43, 'Inro Learn Vue JS', 'Inro Learn Vue JS desc', '../lessonvid/video12.mp4', 12, 'Learn Vue JS'),
+(44, 'intro Angular JS', 'intro Angular JS desc', '../lessonvid/video13.mp4', 13, 'Angular JS'),
+(48, 'Intro to Python Complete', 'This is lesson number 1', '../lessonvid/video11.mp4', 16, 'Python Complete'),
+(49, 'Introduction to React Native', 'This intro video of React native', '../lessonvid/video11.mp4', 17, 'Learn React Native'),
+(51, 'Web design front end developer Bangla Tutorial', ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.', '../lessonvid/web design front end developer Bangla Tutorial.mp4', 18, 'HTML Learning Course');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `stu_id` int(11) NOT NULL,
+  `stu_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `stu_email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `stu_pass` varchar(255) COLLATE utf8_bin NOT NULL,
+  `stu_occ` varchar(255) COLLATE utf8_bin NOT NULL,
+  `stu_img` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`stu_id`, `stu_name`, `stu_email`, `stu_pass`, `stu_occ`, `stu_img`) VALUES
+(172, 'Ant Man', 'ant@example.com', '123456', ' Web Developer', '../image/stu/student4.jpg'),
+(173, ' Dr Strange', 'doc@example.com', '123456', ' Web Developer', '../image/stu/student1.jpg'),
+(176, ' Shaktiman', 'shaktiman@ischool.com', '123456', 'Software ENgg', '../image/stu/shaktiman.jpg'),
+(178, ' Mario', 'mario@ischool.com', '1234567', ' Web Dev', '../image/stu/super-mario-2690254_1280.jpg'),
+(182, ' sonam', 'sonam@gmail.com', '123456', ' Web Dev', '../image/stu/student2.jpg'),
+(183, ' Wahid Rahman', 'wahid@gmail.com', '2000', ' ', '../image/stu/dfdf.png'),
+(184, ' Mehedi', 'mehedi@gmail.com', '1000', ' ', '../image/stu/dfdf.png'),
+(185, ' Raza', 'raza@gmail.com', '3000', 'Student', '../image/stu/adam-hlasny-tcnT-sFk_yU-unsplash.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `courseorder`
+--
+ALTER TABLE `courseorder`
+  ADD PRIMARY KEY (`co_id`),
+  ADD UNIQUE KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`f_id`);
+
+--
+-- Indexes for table `lesson`
+--
+ALTER TABLE `lesson`
+  ADD PRIMARY KEY (`lesson_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`stu_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `courseorder`
+--
+ALTER TABLE `courseorder`
+  MODIFY `co_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `f_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `lesson`
+--
+ALTER TABLE `lesson`
+  MODIFY `lesson_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `stu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=186;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
